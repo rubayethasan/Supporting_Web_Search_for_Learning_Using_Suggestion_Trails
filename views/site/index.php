5b4ddc9fa6f446dce098702aa3fc1b0838784256
Rubayet Hasan <rubayet09csejust@gmail.com>
2021-11-06 20:00:03 +0100

    title changed

--- views/site/index.php
+++ views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 use yii\helpers\Html;
-$this->title = 'Suggestion Manager';
+$this->title = 'SSwST: Supporting Search with Suggestion Trails';
 ?>
 <div class="site-index">
     <div class="breadcrumb">
         <h4>Total process will be executed according to the process flow below. Please go through the diagram below and after understanding start the process.</h4>
-        <a href="<?=Yii::$app->request->baseUrl?>/answers/withoutsearch?u=2345">CLICK ME TO START</a>
+        <a class="btn btn-block btn-success" href="<?=Yii::$app->request->baseUrl?>/answers/withoutsearch?u=<?=time()?>">CLICK ME TO START</a>
     </div>
     <div class="body-content">
         <div style="text-align: center">
